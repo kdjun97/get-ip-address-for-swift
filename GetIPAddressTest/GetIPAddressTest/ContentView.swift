ff48c3fbdc4b2a6bf238da169cd65a022a782082
kdjun97 <kdjun97@gmail.com>
2024-01-01 19:53:59 +0900

    init project

---
+++ GetIPAddressTest/GetIPAddressTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GetIPAddressTest
+//
+//  Created by 김동준 on 1/1/24
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("GET IP Address Test")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GetIPAddressTest/GetIPAddressTest/GetIPAddressTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetIPAddressTestApp.swift
+//  GetIPAddressTest
+//
+//  Created by 김동준 on 1/1/24
+//
+
+import SwiftUI
+
+@main
+struct GetIPAddressTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
